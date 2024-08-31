30be7bafddab9cfb89fc0d331d056355b68f9cb1
xiracuko <matvey.a.a.7@gmail.com>
2024-08-31 10:41:12 +0300

    build: initialization of styles in the project (library/files)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import CanvasBlock from './components/CanvasBlock';
+import "./scss/style.scss"
 
 const App: React.FC = () => {
 
   return (
-    <div className="App">
+    <div className="container">
       <CanvasBlock />
     </div>
   );
